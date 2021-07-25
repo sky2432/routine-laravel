802214483393b951513d050c3640aea4bedb2f16
sky2432 <skyarts0122@gmail.com>
2021-07-25 10:38:14 +0900

    ✨Add: マイグレーション追加

---
+++ src/database/migrations/2021_07_25_012121_create_routines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRoutinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('routines', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('total_rank_id')->constrained('ranks');
+            $table->foreignId('continuous_rank_id')->constrained('ranks');
+            $table->foreignId('recovery_rank_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('routines');
+    }
+}
