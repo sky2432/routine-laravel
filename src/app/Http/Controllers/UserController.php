9cd4500e50ba0f85749988a7687a7c990031188c
sky2432 <skyarts0122@gmail.com>
2021-07-25 10:47:33 +0900

    ✨Add: UserController追加

---
+++ src/app/Http/Controllers/UserController.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $items = User::all();
+        ;
+
+        return response()->json([
+            'data' => $items
+        ], 200);
+    }
+
+    public function store(Request $request)
+    {
+        $item = new User;
+        $item->password = Hash::make($request->password);
+        $item->fill($request->all())->save();
+
+        return response()->json([
+            'data' => $item
+        ], 200);
+    }
+
+    //名前・メールアドレスの更新
+    public function update(Request $request, $user_id)
+    {
+        $item = User::find($user_id);
+        $item->update($request->all());
+
+        return response()->json([
+            'data' => $item
+        ], 200);
+    }
+
+    public function updatePassword(Request $request, $user_id)
+    {
+        $item = User::find($user_id);
+        $item->password = Hash::make($request->new_password);
+        $item->save();
+
+        return response()->json([
+            'data' => $item
+        ], 200);
+    }
+
+    public function destroy($user_id)
+    {
+        User::destroy($user_id);
+
+        return response()->json([], 204);
+    }
+}
